76f3c265f6916d2a639c14445b4b4dd7ffec5744
gisenav <nidiagisenav@gmail.com>
2024-07-05 01:31:08 -0300

    agregue suscripciones

--- database/suscripciones.sql
+++ database/suscripciones.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: suscripciones
+-- Host: localhost    Database: suscripciones
 -- ------------------------------------------------------
 -- Server version	5.5.5-10.4.32-MariaDB
 
@@ -78,7 +78,7 @@ CREATE TABLE `suscripciones` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `suscripciones` varchar(40) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -87,6 +87,7 @@ CREATE TABLE `suscripciones` (
 
 LOCK TABLES `suscripciones` WRITE;
 /*!40000 ALTER TABLE `suscripciones` DISABLE KEYS */;
+INSERT INTO `suscripciones` VALUES (1,'ultimas noticias'),(2,'zonas peligrosas'),(3,'recomendaciones'),(4,'todo');
 /*!40000 ALTER TABLE `suscripciones` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -99,4 +100,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-07-04 21:01:49
+-- Dump completed on 2024-07-05  1:21:47
